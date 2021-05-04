fbd5073fdb5ac4cc74ecdba3220428874e42e49a
Riadhi Aditya <ridzeal@gmail.com>
2021-05-04 09:26:02 +0700

    Minor fix on source path in mock script

--- mock/script.sh
+++ mock/script.sh
@@ -4,4 +4,4 @@ go get github.com/golang/mock/gomock
 go install github.com/golang/mock/mockgen
 
 # lib
-mockgen -package=mock -destination=mock/mock_response_writer.go -source=/src/net/http/server.go
+mockgen -package=mock -destination=mock/mock_response_writer.go -source=$GOROOT/src/net/http/server.go
